c52dec460105a3845f698ac78a923b68b8f2fbbc
doudou19911223 <18767117240@139.com>
2018-06-10 21:05:34 +0800

    更新

--- 02.py
+++ 02.py
@@ -1,3 +1,14 @@
 
 #print
 print(19)
+class Student():
+    name = "dana"
+    age = 18
+    # 注意say的写法，参数由一个self
+    def say(self):
+        self.name = "aaaa"
+        self.age = 200
+        print("my name is {0}".format(self.name))
+        print("my age is {0}".format(self.age))
+yueyue = Student()
+yueyue.say()
